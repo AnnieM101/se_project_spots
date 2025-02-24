4461fe5fe10ec1d54491ee2f7f8ae4fc194385ea
AnnieM101 <annacarolinekatherine@gmail.com>
2025-02-24 04:25:31 -0500

    Refined JS to eliminate unnecessary hard coding and improve performance

--- scripts/index.js
+++ scripts/index.js
@@ -25,12 +25,31 @@ const initialCards = [
   },
 ];
 
-const profileEditButton = document.querySelector(".profile__edit-button");
+const editFormElement = document.querySelector(".modal__container");
 
 const editModal = document.querySelector("#edit-profile-modal");
+
 const editModalCloseButton = editModal.querySelector(".modal__close-button");
 
+const editModalNameInput = editModal.querySelector("#profile-name-input");
+
+const editModalDescriptionInput = editModal.querySelector(
+  "#profile-description-input"
+);
+
+const profileName = document.querySelector(".profile__name");
+
+const profileDescription = document.querySelector(".profile__description");
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const cardTemplate = document.querySelector("#card-template");
+
+const cardsList = document.querySelector(".cards__list");
+
 function openModal() {
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
   editModal.classList.add("modal_opened");
 }
 
@@ -38,6 +57,33 @@ function closeModal() {
   editModal.classList.remove("modal_opened");
 }
 
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__caption");
+  cardNameEl.textContent = data.name;
+
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = data.link;
+
+  return cardElement;
+}
+
 profileEditButton.addEventListener("click", openModal);
 
 editModalCloseButton.addEventListener("click", closeModal);
+
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
