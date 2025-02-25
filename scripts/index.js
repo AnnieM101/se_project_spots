ab37f9a9c11c9e2cbf02af6927e49e443e6c8b70
AnnieM101 <annacarolinekatherine@gmail.com>
2025-02-25 01:06:48 -0500

    Updated JS

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,36 @@ const initialCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+    alt: "A photo of a scenic window that overlooks a snowy mountain town",
   },
   {
     name: "Restaurant terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+    alt: "A photo of a quaint café",
   },
   {
     name: "An outdoor cafe",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    alt: "A photo of a busy café's outdoor dining area",
   },
   {
     name: "A very long bridge, over the forest and through the trees",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+    alt: "A photo of a vast wooden and metal suspension bridge over lush forest",
   },
   {
     name: "Tunnel with morning light",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    alt: "A photo of a tunnel",
   },
   {
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    alt: "A photo of a scenic log cabin covered in snow",
   },
 ];
 
-const editFormElement = document.querySelector(".modal__container");
+const editFormElement = document.querySelector(".modal__form");
 
 const editModal = document.querySelector("#edit-profile-modal");
 
@@ -61,6 +67,7 @@ function handleEditFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = editModalNameInput.value;
   profileDescription.textContent = editModalDescriptionInput.value;
+  closeModal();
 }
 
 function getCardElement(data) {
@@ -68,11 +75,16 @@ function getCardElement(data) {
     .querySelector(".card")
     .cloneNode(true);
 
+  const cardImageAlt = cardElement.querySelector(".card__image");
+  cardImageAlt.alt = data.alt;
+
   const cardNameEl = cardElement.querySelector(".card__caption");
   cardNameEl.textContent = data.name;
 
   const cardImage = cardElement.querySelector(".card__image");
   cardImage.src = data.link;
+
+
 
   return cardElement;
 }
